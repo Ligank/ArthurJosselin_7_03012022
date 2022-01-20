ecbe6835c75b059c4195a80ef47eefc9e4f2296f
Ligank <Arthur.Josselin@hotmail.fr>
2022-01-20 14:20:33 +0100

    branche native

--- js/filtre_recherche.js
+++ js/filtre_recherche.js
@@ -1,9 +1,26 @@
+import FiltreRechercheTags from "./filtre_recherche_tags.js";
 export default class FiltreRecherche {
     filtreTitre() {
         let barreRecherche = document.querySelector(".recherche_barre");
         barreRecherche.addEventListener('input', function() {
             if (barreRecherche.value.length > 2) {
                 console.log("test");
+
+
+
+
+
+
+
+
+
+            } else {
+                recette.forEach( element => {
+                    element.style.display = "block";
+                    element.classList.add("actifrecherche");
+                })
+                document.querySelector(".aucune_recette").style.display ="none";
+                new FiltreRechercheTags().cacherTag();
             }
         })
     }
