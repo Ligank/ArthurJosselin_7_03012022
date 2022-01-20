d3e07877c06b49e78047096748574006055023a0
Ligank <Arthur.Josselin@hotmail.fr>
2022-01-20 14:34:30 +0100

    branche native

--- js/filtre_recherche.js
+++ js/filtre_recherche.js
@@ -1,10 +1,13 @@
 import FiltreRechercheTags from "./filtre_recherche_tags.js";
 export default class FiltreRecherche {
     filtreTitre() {
+        let recette = document.querySelectorAll(".recette");
         let barreRecherche = document.querySelector(".recherche_barre");
         barreRecherche.addEventListener('input', function() {
             if (barreRecherche.value.length > 2) {
-                console.log("test");
+                for(let i = 0; i < recette.length; i++) {
+                    console.log("test")
+                }
 
 
 
@@ -15,10 +18,11 @@ export default class FiltreRecherche {
 
 
             } else {
-                recette.forEach( element => {
-                    element.style.display = "block";
-                    element.classList.add("actifrecherche");
-                })
+                for(let i = 0; i < recette.length; i++) {
+                    console.log(recette[i])
+                    //recette[i].style.display = "block";
+                    //recette[i].classList.add("actifrecherche");
+                }
                 document.querySelector(".aucune_recette").style.display ="none";
                 new FiltreRechercheTags().cacherTag();
             }
